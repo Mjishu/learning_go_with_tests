b5de17649e35a5b3970fd5d7d3d93b454b4c1f5e
mjishu <128941980+Mjishu@users.noreply.github.com>
2024-11-10 16:11:39 -0500

    finished structs, worked through interfaces and structs, as well as how to make methods and how to make them more readable

--- arraysAndSlices/arrays_test.go
+++ arraysAndSlices/arrays_test.go
@@ -1,6 +1,9 @@
 package arraySlices
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestSum(t *testing.T) {
 	t.Run("collection of size 5", func(t *testing.T) {
@@ -10,6 +13,17 @@ func TestSum(t *testing.T) {
 		want := 15
 
 		assertCorrectMessage(t, got, want, numbers)
+	})
+}
+
+func TestSumAll(t *testing.T) {
+	t.Run("Runs test as expected", func(t *testing.T) {
+		got := SumAll([]int{3, 2}, []int{0, 9})
+		want := []int{5, 9}
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("Got %v but wanted %v", got, want)
+		}
 	})
 }
 
